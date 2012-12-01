de49953040cfcf2a91fe0df1f5b606d169bde3d7
Derek <heyderek@dereknelson.net>
2012-12-01 15:14:45 -0800

    Added headers and footers for wordpress.

--- index.php
+++ index.php
@@ -1,75 +1,32 @@
-<!DOCTYPE html>
-<meta charset="utf-8">
-<html>
-  <head>
-    <link rel="stylesheet" href="style.css" title="stylesheet" type="text/css" media="screen" charset="utf-8">
-    <link rel="shortcut icon" href="images/favicon.ico" />
-    <script src="js/jquery.js" charset="utf-8"></script>
-    <!--[if lt IE 9]><script src="js/html5.js" charset="utf-8"></script><![endif]-->
-    <script src="js/custom.js" charset="utf-8"></script>
-    <script src="js/prefixfree.min.js" charset="utf-8"></script>
-    <title>Mid Columbia Warehouses</title>
-  </head>
-  <body>
-    <div class="page">
-      <section class="masthead">
-        <nav class="menu">
-          <div class="wrapper"><a href="#" class="logo"><img src="images/logo.png" alt="Mid-Columbia Warehouses Logo Graphic" /></a></div><!-- /.wrapper -->
-          <ul class="wrapper">
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-          </ul>
-        </nav><!-- /.menu -->
-        <header class="wrapper">
-          <hgroup class="branding">
-            <h1>Mid-Columbia Warehouses</h1>
-            <h2>Complete Storage Facilities</h2>
-          </hgroup><!-- /.branding -->
-          <div class="location">
-            <p>509-547-2194</p><!-- /.phone -->
-            <address>1810 Ainsworth, Pasco, WA 99301</address>
-          </div><!-- /.location -->
-        </header><!-- /.wrapper -->
-        <section class="featured-content wrapper">
-          <div class="primary content">
-            <article class="caption">
-              <h3>Complete Facilities with a Strategic Location</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            </article><!-- /.caption -->
-          </div><!-- /.primary -->
-            <div class="secondary">
-              <div class="ns-border-wrap"><img src="images/stockholder.jpg" /></div><!-- /.ns-border-wrap -->
-            </div><!-- /.secondary-->
-        </section><!-- /.featured-content -->
-      </section><!-- /.masthead -->
-      <section class="colophon content">
-        <footer class="wrapper">
-          <aside>
-            <h3>Address</h3>
-            <address><span>Physical Address:</span> 1810 E Ainsworth, Pasco, WA 99301</address>
-            <address><span>Physical Address:</span> 1810 E Ainsworth, Pasco, WA 99301</address>
-            <p class="phone"><span>Phone:</span> 509-547-7761</p>
-            <p class="phone"><span>Fax:</span> 509-547-2194</p>
-          </aside>
-          <aside>
-            <h3>Contact</h3>
-            <ul>
-              <li><a href="#">Customer Support</a></li>
-              <li><a href="#">Accounting</a></li>
-              <li><a href="#">General Information</a></li>
-            </ul>
-          </aside>
-          <a href="#" class="logo">
-            <img src="images/footer_logo.png" alt="Mid-Columbia Warehouses Logo Graphic" />
-          </a><!-- /.logo -->
-        </footer><!-- /.wrapper -->
-      </section><!-- /.colophon -->
-      <footer class="credits wrapper">
-          <p>&copy 2012 Mid-Columbia Warehouses.  Brought to you by <a href="#">Rockwalla IT.</a></p>
-      </footer><!-- /.credits  -->
-    </div><!-- /.page -->
-  </body>
-</html>
+<?php get_header(); ?>
+      <section class="two-column">
+        <div class="wrapper content">
+          <section class="secondary">
+            <aside>
+              <h3>Secondary Heading</h3>
+              <ul>
+                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</li>
+                <li>Item 1</li>
+                <li>Item 1</li>
+                <li>Item 1</li>
+              </ul>
+            </aside>
+            <aside>
+              <h3>Secondary Heading</h3>
+              <ul>
+                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</li>
+                <li>Item 1</li>
+                <li>Item 1</li>
+                <li>Item 1</li>
+              </ul>
+            </aside>
+          </section><!-- /.secondary -->
+          <section class="primary">
+            <article>
+              <h1>Article Title heading.</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </article>
+          </section><!-- /.primary -->
+        </div><!-- /.wrapper -->
+      </section><!-- /.two-column -->
+      <?php get_footer(); ?>
