9e15431dc310ef5ea3a67bf8967235b38f0e44cf
Derek <heyderek@dereknelson.net>
2012-12-01 15:15:06 -0800

    Adding files for Wordpress.

---
+++ footer.php
@@ -0,0 +1,28 @@
+      <section class="colophon content">
+        <footer class="wrapper">
+          <aside>
+            <h3>Address</h3>
+            <address><span>Physical Address:</span> 1810 E Ainsworth, Pasco, WA 99301</address>
+            <address><span>Physical Address:</span> 1810 E Ainsworth, Pasco, WA 99301</address>
+            <p class="phone"><span>Phone:</span> 509-547-7761</p>
+            <p class="phone"><span>Fax:</span> 509-547-2194</p>
+          </aside>
+          <aside>
+            <h3>Contact</h3>
+            <ul>
+              <li><a href="#">Customer Support</a></li>
+              <li><a href="#">Accounting</a></li>
+              <li><a href="#">General Information</a></li>
+            </ul>
+          </aside>
+          <a href="#" class="logo">
+            <img src="<?php bloginfo('template_url'); ?>/images/footer_logo.png" alt="Mid-Columbia Warehouses Logo Graphic" />
+          </a><!-- /.logo -->
+        </footer><!-- /.wrapper -->
+      </section><!-- /.colophon -->
+      <footer class="credits wrapper">
+          <p>&copy 2012 Mid-Columbia Warehouses.  Brought to you by <a href="#">Rockwalla IT.</a></p>
+      </footer><!-- /.credits  -->
+    </div><!-- /.page -->
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html>
+  <head>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" title="stylesheet" type="text/css" media="screen" charset="utf-8">
+    <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" />
+    <script src="js/jquery.js" charset="utf-8"></script>
+    <!--[if lt IE 9]><script src="<?php bloginfo('template_url'); ?>/js/html5.js" charset="utf-8"></script><![endif]-->
+    <script src="<?php bloginfo('template_url'); ?>/js/custom.js" charset="utf-8"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/prefixfree.min.js" charset="utf-8"></script>
+    <title>
+      <?php
+        //Title based on page.
+        global $page, $paged;
+        wp_title( '|', true, 'right' );
+        // Add the blog name.
+        bloginfo( 'name' );
+        // Add the blog description for the home/front page.
+        $site_description = get_bloginfo( 'description', 'display' );
+        if ( $site_description && ( is_home() || is_front_page() ) )
+        	echo " | $site_description";
+        // Add a page number if necessary:
+        if ( $paged >= 2 || $page >= 2 )
+        	echo ' | ' . sprintf( __( 'Page %s', 'toolbox' ), max( $paged, $page ) ); 
+      ?>
+    </title>
+  </head>
+  <body>
+    <div class="page">
+      <section class="masthead">
+        <nav class="menu">
+          <div class="wrapper"><a href="#" class="logo"><img src="<?php bloginfo('template_url'); ?>/images/logo.png" alt="Mid-Columbia Warehouses Logo Graphic" /></a></div><!-- /.wrapper -->
+          <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'wrapper')); ?>
+        </nav><!-- /.menu -->
+        <header class="wrapper">
+          <hgroup class="branding">
+            <h1>Mid-Columbia Warehouses</h1>
+            <h2>Complete Storage Facilities</h2>
+          </hgroup><!-- /.branding -->
+          <div class="location">
+            <p>509-547-2194</p><!-- /.phone -->
+            <address>1810 Ainsworth, Pasco, WA 99301</address>
+          </div><!-- /.location -->
+        </header><!-- /.wrapper -->
+        <?php if(is_home() || is_front_page()) : ?>
+          <section class="featured-content wrapper">
+            <div class="primary content">
+              <article class="caption">
+                <h3>Complete Facilities with a Strategic Location</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              </article><!-- /.caption -->
+            </div><!-- /.primary -->
+              <div class="secondary">
+                <div class="ns-border-wrap"><img src="<?php bloginfo('template_url'); ?>/images/stockholder.jpg" /></div><!-- /.ns-border-wrap -->
+              </div><!-- /.secondary-->
+          </section><!-- /.featured-content -->
+        <?php endif; ?>
+      </section><!-- /.masthead -->
